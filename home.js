51d8402b8c7ca3206f457b69d922faabb05d9d77
mbmugdho <mdmontasirbillah5@gmail.com>
2025-08-26 12:32:10 +0600

    Updated Files

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
 const validPin = 1234
 const transactionData = []
 
+// logout button 
+// document.getElementById('logout-btn').addEventListener('click', function (){})
+
 // function to get multiple input values in one line
 function getInputValueNumber(id) {
   const inputField = document.getElementById(id)
@@ -49,24 +52,15 @@ function handleToggle(id) {
  }
 
 // add money feature
-document.getElementById('btn-addMoney').addEventListener('click', function (e) {
+document.getElementById('btn-addMoney')
+.addEventListener('click', function (e) {
   e.preventDefault()
   const bank = getInputValue('bank')
-
   const bankAccount = document.getElementById('bank-account').value
-
   const addAmount = getInputValueNumber('add-amount')
-  if (addAmount <= 0) {
-    alert('Invalid Amount')
-    return
-  }
-
   const addPin = getInputValueNumber('add-pin')
-
   const totalBalance = getInnerText('totalAmount')
-
   const totalNewBalance = addAmount + totalBalance
-
   setInnerText(totalNewBalance)
 
   const data = {
@@ -74,7 +68,10 @@ document.getElementById('btn-addMoney').addEventListener('click', function (e) {
     date:new Date().toLocaleDateString()
   }
   transactionData.push(data)
-
+  if (addAmount <= 0) {
+    alert('Invalid Amount')
+    return
+  }
   if (bankAccount.length < 11) {
     alert('Please Provide Valid Account Number')
     return
@@ -93,18 +90,93 @@ document
   .getElementById('withdraw-button')
   .addEventListener('click', function (e) {
     e.preventDefault()
+    const agentNumber = document.getElementById('agent-number').value
     const amount = getInputValueNumber('withdraw-amount')
-
+    const withdrawPin = getInputValueNumber('withdraw-pin')
     const availableBalance = getInnerText('totalAmount')
+
+        if (agentNumber.length < 11) {
+    alert('Please Provide Valid Account Number')
+    return
+  }
+  if (withdrawPin !== validPin) {
+    alert('Please Provide Correct Pin')
+    return
+  }
     if (amount <=0 || amount > availableBalance) {
       alert("Invalid Amount")
       return
     }
+  
     const totalnewavailableBalance = availableBalance - amount
 
     setInnerText(totalnewavailableBalance)
     const data = {
     name: "Cash out",
+    date:new Date().toLocaleDateString()
+  }
+  transactionData.push(data)
+  })
+// transfer money section 
+document
+  .getElementById('sendnow-button')
+  .addEventListener('click', function (e) {
+    e.preventDefault()
+    const userAccNumber = document.getElementById('user-acc-number').value
+    const amount = getInputValueNumber('transfer-amount')
+    const transferPin = getInputValueNumber('transfer-pin')
+    const availableBalance = getInnerText('totalAmount')
+
+        if (userAccNumber.length < 11) {
+    alert('Please Provide Valid Account Number')
+    return
+  }
+  if (transferPin !== validPin) {
+    alert('Please Provide Correct Pin')
+    return
+  }
+    if (amount <=0 || amount > availableBalance) {
+      alert("Invalid Amount")
+      return
+    }
+  
+    const totalnewavailableBalance = availableBalance - amount
+    setInnerText(totalnewavailableBalance)
+    const data = {
+    name: "Cash out",
+    date:new Date().toLocaleDateString()
+  }
+  transactionData.push(data)
+  })
+
+  // paybill section 
+  document
+  .getElementById('bill-btn')
+  .addEventListener('click', function (e) {
+    e.preventDefault()
+    const bill = getInputValue('bill')
+    const billerAccNumber = document.getElementById('biller-acc-number').value
+    const billAmount = getInputValueNumber('bill-amount')
+    const billPin = getInputValueNumber('bill-pin')
+    const availableBalance = getInnerText('totalAmount')
+
+  if (billerAccNumber.length < 11) {
+    alert('Please Provide Valid Account Number')
+    return
+  }
+  if (billPin !== validPin) {
+    alert('Please Provide Correct Pin')
+    return
+  }
+  if (billAmount <=0 || billAmount > availableBalance) {
+    alert("Invalid Amount")
+    return
+  }
+  
+    const totalnewavailableBalance = availableBalance - billAmount
+    setInnerText(totalnewavailableBalance)
+    const data = {
+    name: "Pay Bill",
     date:new Date().toLocaleDateString()
   }
   transactionData.push(data)
